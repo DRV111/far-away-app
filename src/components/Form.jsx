167480fa4db5977f7310e36f4bfb91fa7015dfd1
DRV111 <xblackcrownx@gmail.com>
2023-10-18 14:55:50 +0300

    fix: fix Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,26 @@
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <div className="form-header">
         <h3>What do you need for your trip? 🎒</h3>
       </div>
       <div className="form-input">
-        <input type="text" placeholder="Add Item" />
-        <input type="number" placeholder="Quantity" />
+        <select>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="Item..." />
         <input type="submit" value="Add Item" />
       </div>
-    </div>
+    </form>
   );
 }
 
